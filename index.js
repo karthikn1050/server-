3331cd3f4c7611486ecf04cc1fe2ae1bdb650b8e
Karthikeyan <karthikn1050@gmail.com>
2021-09-23 17:00:59 +0530

    make it betterss

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000 ;
+
 const cors = require('cors')
 const NSEAPI = require('./API');
+const process = require('process');
+const PORT = process.env.PORT || 5000 ;
+
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+  // Application specific logging, throwing an error, or other logic here
+});
+app.get('/market_status', (req, res) => {
+NSEAPI.getMarketStatus().then((response) => {
+  res.json(response.data);
+}); 
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
-
-
+  
 app.use(
   cors({
     origin:"*"
   })
 )
-app.get('/market_status', (req, res) => {
-  NSEAPI.getMarketStatus()
-      .then(function (response) {
-        res.json(response.data);
-      }).catch(err => {
-        console.log(err);
-        res.sendStatus(500);});
-});
+
 
 app.get('/stocks_info', (req, res) => {
   NSEAPI.getQuoteInfo(req.query.companyName)
